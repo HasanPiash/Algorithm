57b6ea185223cc4f707c89d145efdf7d423ac9b0
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-10-17 22:42:28 +0600

    Create BFS Traversal

---
+++ 005_BFS_Traversal.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>V[1005];
+bool vis[1005];
+void bfs(int src)
+{
+    queue<int>Q;
+    Q.push(src);
+    vis[src]=true;
+    while(!Q.empty())
+    {
+        int par=Q.front();
+        Q.pop();
+        cout<<par<<endl;
+        for(int child:V[par])
+        {
+            if(vis[child]==false)
+            {
+                Q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+int main()
+{
+    int N,E;
+    cin>>N>>E;
+    while(E--)
+    {
+        int A,B;
+        cin>>A>>B;
+        v[A].push_back(B);
+        v[B].push_back(A);
+    }
+    int src;
+    cin >> src;
+    memset(vis, false, sizeof(vis));
+    bfs(src);
+    return 0;
+}
