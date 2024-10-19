9055cc7ff6290775af35eca270bd63ce41efa7b9
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-10-19 15:57:41 +0600

    Create BFS with Level Pair

---
+++ 006_BFS_with_Level_Pair.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int>V[1005];
+bool vis[1005];
+void bfs(int src,int des)
+{
+    queue<pair<int,int>>Q;
+    Q.push({src,0});
+    vis[src]=true;
+    bool paisi=false;
+    while(!Q.empty())
+    {
+        pair<int,int>P=Q.front();
+        Q.pop();
+        int par=P.first;
+        int level=P.second;
+        if(par==des)
+        {
+            cout<<level<<endl;
+            paisi=true;
+        }
+        for(int child:V[par])
+        {
+            if(vis[child]==false)
+            {
+                Q.push({child,level+1});
+                vis[child]=true;
+            }
+        }
+    }
+    if(paisi==false)
+    {
+        cout<<-1<<endl;
+    }
+}
+int main()
+{
+    int N,E;
+    cin>>N>>E;
+    while(E--)
+    {
+        int A,B;
+        cin>>A>>B;
+        V[A].push_back(B);
+        V[B].push_back(A);
+    }
+    int src;
+    cin>>src;
+    memset(vis,false,sizeof(vis));
+    bfs(src,9);
+    return 0;
+}
