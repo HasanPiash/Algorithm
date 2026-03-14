be10b82b4928ced3a97b4ef20469262f0a21cfa0
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-14 15:59:08 +0600

    Update 011_DFS_ON_2D.cpp

--- 011_DFS_ON_2D.cpp
+++ 011_DFS_ON_2D.cpp
@@ -4,12 +4,15 @@ char A[20][20];
 bool vis[20][20];
 vector<pair<int,int>>D={{0,1},{0,-1},{-1,0},{1,0}};
 int N,M;
+
 bool valid(int i, int j)
 {
     if(i<0 || i>=N || j<0 || j>=M)
         return false;
+    
     return true;
 }
+
 void dfs(int si,int sj)
 {
     cout<<si<<" "<<sj<<endl;
@@ -19,15 +22,18 @@ void dfs(int si,int sj)
         int ci=si+D[i].first;
         int cj=sj+D[i].second;
         if (valid(ci,cj)==true && vis[ci][cj]==false)
+            
         {
             dfs(ci,cj);
         }
     }
 }
+
 int main()
 {
     cin>>N>>M;
     for(int i=0; i<N; i++)
+        
     {
         for(int j=0; j<M; j++)
         {
@@ -38,5 +44,6 @@ int main()
     cin>>si>>sj;
     memset(vis,false,sizeof(vis));
     dfs(si,sj);
+    
     return 0;
 }
