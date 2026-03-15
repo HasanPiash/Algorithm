8dd4d37a2027a4f0d302e88725c0cb96b2a75c08
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-15 22:31:55 +0600

    Update 012_Component.cpp

--- 012_Component.cpp
+++ 012_Component.cpp
@@ -8,6 +8,7 @@ void dfs(int src)
 {
     cout<<src<<endl;
     vis[src]=true;
+    
     for(int child:v[src])
     {
         if(vis[child]==false)
@@ -28,6 +29,7 @@ int main()
     }
     memset(vis,false,sizeof(vis));
     int c=0;
+    
     for(int i=0; i<n; i++)
     {
         if(vis[i]==false)
