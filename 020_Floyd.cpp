14d2c929315f20c473ea268c71fc143ad9e89b19
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-22 22:28:39 +0600

    Update 020_Floyd.cpp

--- 020_Floyd.cpp
+++ 020_Floyd.cpp
@@ -6,6 +6,7 @@ int main()
     ll n,e;
     cin>>n>>e;
     ll adj[n][n];
+    
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
@@ -15,12 +16,14 @@ int main()
                 adj[i][j]=0;
         }
     }
+    
     while(e--)
     {
         int a,b,c;
         cin>>a>>b>>c;
         adj[a][b]=c;
     }
+    
     for(int k=0; k<n; k++)
     {
         for(int i=0; i<n; i++)
