3e0518f001321f89c079281108d9f1e4d2439eca
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-23 22:33:05 +0600

    Update 025_Find_if_Path_Exists_in_Graph.cpp

--- 025_Find_if_Path_Exists_in_Graph.cpp
+++ 025_Find_if_Path_Exists_in_Graph.cpp
@@ -8,6 +8,7 @@ public:
     void dfs(int s)
     {
         vis[s]=true;
+        
         for(int child:v[s])
         {
             if(!vis[child])
@@ -19,6 +20,7 @@ public:
     bool validPath(int n,vector<vector<int>> &edges,int source,int d)
     {
         memset(vis,false,sizeof(vis));
+        
         for(int i=0; i<edges.size(); i++)
         {
             int a=edges[i][0];
@@ -27,6 +29,7 @@ public:
             v[b].push_back(a);
         }
         dfs(source);
+        
         return vis[d];
     }
 };
