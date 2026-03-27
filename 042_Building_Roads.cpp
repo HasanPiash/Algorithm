eb7acafd263a934b94327d387c3563281980509d
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-27 20:59:33 +0600

    Update 042_Building_Roads.cpp

--- 042_Building_Roads.cpp
+++ 042_Building_Roads.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 const int N=1e5+5;
 int parent[N];
@@ -17,12 +18,14 @@ int dsu_find(int node)
         return node;
     int leader=dsu_find(parent[node]);
     parent[node]=leader;
+    
     return leader;
 }
 void dsu_union_by_size(int node1,int node2)
 {
     int leaderA=dsu_find(node1);
     int leaderB=dsu_find(node2);
+    
     if(group_size[leaderA]>group_size[leaderB])
     {
         parent[leaderB]=leaderA;
@@ -39,6 +42,7 @@ int main()
     int n,e;
     cin>>n>>e;
     dsu_initialize(n);
+    
     while(e--)
     {
         int a,b;
@@ -51,6 +55,7 @@ int main()
         }
     }
     vector<int>leader;
+    
     for(int i=1; i<=n; i++)
     {
         leader.push_back(dsu_find(i));
@@ -58,6 +63,7 @@ int main()
     sort(leader.begin(),leader.end());
     leader.erase(unique(leader.begin(),leader.end()),leader.end());
     cout<<leader.size()-1<<endl;
+    
     for(int i=0; i<leader.size()-1; i++)
     {
         cout<<leader[i]<<" "<<leader[i+1]<<endl;
