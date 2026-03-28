607250ff6be68660ffdb031b4122c60cffec995a
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-28 22:48:48 +0600

    Update 047_Sundarban.cpp

--- 047_Sundarban.cpp
+++ 047_Sundarban.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 char a[50][50];
 bool vis[50][50];
@@ -18,14 +19,17 @@ void bfs(int si,int sj)
     q.push({si,sj});
     dis[si][sj]=0;
     vis[si][sj]=true;
+    
     while(!q.empty())
     {
         pair<int,int>par=q.front();
         q.pop();
+        
         for(int i=0; i<4; i++)
         {
             int ci=par.first+d[i].first;
             int cj=par.second+d[i].second;
+            
             if(valid(ci,cj) && !vis[ci][cj] && a[ci][cj] !='T')
             {
                 q.push({ci,cj});
@@ -41,6 +45,7 @@ int main()
     d.push_back({0,-1});
     d.push_back({-1,0});
     d.push_back({1,0});
+    
     while(cin>>n)
     {
         int si,sj;
@@ -50,6 +55,7 @@ int main()
             for(int j=0; j<n; j++)
             {
                 cin>>a[i][j];
+                
                 if(a[i][j]=='S')
                 {
                     si=i;
