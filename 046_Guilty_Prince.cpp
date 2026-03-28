91fdb8a86e68710a67b8349d4d00a06c0c62d79b
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-28 22:46:59 +0600

    Update 046_Guilty_Prince.cpp

--- 046_Guilty_Prince.cpp
+++ 046_Guilty_Prince.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 const int N=25;
 char a[N][N];
@@ -17,10 +18,12 @@ void dfs(int si,int sj)
 {
     vis[si][sj]=true;
     cnt++;
+    
     for(int i=0; i<4; i++)
     {
         int ci=si+d[i].first;
         int cj=sj+d[i].second;
+        
         if(valid(ci,cj) && !vis[ci][cj] && a[ci][cj]=='.')
         {
             dfs(ci,cj);
@@ -32,10 +35,12 @@ int main()
     int t;
     cin>>t;
     int cs=1;
+    
     while(t--)
     {
         cin>>m>>n;
         int si,sj;
+        
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
